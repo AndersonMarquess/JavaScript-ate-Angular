4280c1f08a23c40feb1ee13482cd43ddfb8ca884
unknown <andersongabriel96.ag@gmail.com>
2019-06-04 14:37:25 -0300

    Exibindo o imc com For e Function

--- Curso 1/introducao-javascript/js/index.js
+++ Curso 1/introducao-javascript/js/index.js
@@ -2,7 +2,25 @@
 var titulo = document.querySelector(".titulo-principal");
 // Imprime os dados com a tag
 console.log(titulo);
-// Impre apenas o conteúdo da tag
+// Imprime apenas o conteúdo da tag
 console.log(titulo.textContent);
 
 document.querySelector("title").innerText = "little italy? never heard about...";
+
+//For com query dos pacientes para calcular automaticamente os IMC's
+
+var pacientes = document.querySelectorAll(".paciente");
+for(var i = 0; i < pacientes.length; i++) {
+    mostrarIMCnoTD(pacientes[i]);
+}
+
+function mostrarIMCnoTD(paciente) {
+    if(paciente != null) {
+        var peso = paciente.querySelector(".info-peso").textContent;
+        var altura = paciente.querySelector(".info-altura").textContent;
+        var imc = peso / (altura * altura);
+        
+        //Definir quantidade de casas decimais
+        paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+    }
+}
