21c385c42c0dc4b3a4e9116428ee92351bc99aa2
unknown <andersongabriel96.ag@gmail.com>
2019-06-04 15:07:05 -0300

    Adicionando classe ao elemento

--- Curso 1/introducao-javascript/js/index.js
+++ Curso 1/introducao-javascript/js/index.js
@@ -10,17 +10,23 @@ document.querySelector("title").innerText = "little italy? never heard about..."
 //For com query dos pacientes para calcular automaticamente os IMC's
 
 var pacientes = document.querySelectorAll(".paciente");
-for(var i = 0; i < pacientes.length; i++) {
+
+for (var i = 0; i < pacientes.length; i++) {
     mostrarIMCnoTD(pacientes[i]);
+
+    if (i % 2 == 0) {
+        // muda a cor da linha, adicionando uma classe ao elemento.
+        pacientes[i].classList.add("campo-invalido");
+    }
 }
 
 function mostrarIMCnoTD(paciente) {
-    if(paciente != null) {
+    if (paciente != null) {
         var peso = paciente.querySelector(".info-peso").textContent;
         var altura = paciente.querySelector(".info-altura").textContent;
         var imc = peso / (altura * altura);
-        
-        //Definir quantidade de casas decimais
+
+        //toFixed é usado para definir a quantidade de casas decimais
         paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
     }
 }
