a4faa8d04c2ba710c1e83e11271089f0222124e5
unknown <andersongabriel96.ag@gmail.com>
2019-06-04 18:04:11 -0300

    Event Listener para captura click.

---
+++ Curso 1/introducao-javascript/js/evento.js
@@ -0,0 +1,17 @@
+var titulo = document.querySelector("h1");
+
+//Escuta evento -> função invocada
+titulo.addEventListener("click", imprimirEvento);
+function imprimirEvento() {
+    console.log("clicou");
+}
+
+// função anônima que detecta duplo click
+titulo.addEventListener("dblclick", () => confirm("Deu certo!"));
+
+var btnAdicionar = document.querySelector("#adicionar-paciente");
+btnAdicionar.addEventListener("click", (event) => { 
+    //para impedir o comportamento padrão de reload da página, usamos o preventDefault()
+    event.preventDefault();
+    console.log("Adicionar paciente?");
+});
