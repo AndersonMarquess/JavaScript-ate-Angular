cef6148febd05f569b18e762e5cd3058deba0792
unknown <andersongabriel96.ag@gmail.com>
2019-06-04 21:02:49 -0300

    Criando elementos html e adicionando elementos filhos
    Alterado comportamento padrão do botão dentro do form

--- Curso 1/introducao-javascript/js/evento.js
+++ Curso 1/introducao-javascript/js/evento.js
@@ -10,8 +10,41 @@ function imprimirEvento() {
 titulo.addEventListener("dblclick", () => confirm("Deu certo!"));
 
 var btnAdicionar = document.querySelector("#adicionar-paciente");
-btnAdicionar.addEventListener("click", (event) => { 
+btnAdicionar.addEventListener("click", (event) => {
     //para impedir o comportamento padrão de reload da página, usamos o preventDefault()
     event.preventDefault();
-    console.log("Adicionar paciente?");
+    adicionarPacienteNaTabela();
+    console.log("Paciente adicionado.");
 });
+
+var form = document.querySelector("#form-add-paciente");
+function adicionarPacienteNaTabela() {
+    //é possível acessar os input's dentro do form, através do nome. -> form.nameDoInput
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTR = document.createElement("tr");
+    adicionarValorNaTR(nome, pacienteTR);
+    adicionarValorNaTR(peso, pacienteTR);
+    adicionarValorNaTR(altura, pacienteTR);
+    adicionarValorNaTR(gordura, pacienteTR);
+
+    adicionarTRnaTabela(pacienteTR);
+}
+
+function adicionarTRnaTabela(tr) {
+    document.querySelector("#tabela-pacientes")
+        .appendChild(tr);
+}
+
+function adicionarValorNaTR(valor, tr) {
+    //Cria o elemento td
+    var td = document.createElement("td");
+    //adiciona o valor dentro da td
+    td.textContent = valor;
+    //adiciona o 
+    tr.appendChild(td);
+}
+
