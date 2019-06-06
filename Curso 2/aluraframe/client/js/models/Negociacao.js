2b1802c4b34f384bae1da1c026c747129949b8e8
unknown <andersongabriel96.ag@gmail.com>
2019-06-06 18:53:10 -0300

    Adicionando técnicas de programação defensiva em objetos da classe.

--- Curso 2/aluraframe/client/js/models/Negociacao.js
+++ Curso 2/aluraframe/client/js/models/Negociacao.js
@@ -7,7 +7,7 @@ class Negociacao {
         /**
          * _data -> o underscore é uma convenção para que o valor da variável não seja alterada fora da classe.
          */
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         /**
@@ -19,14 +19,19 @@ class Negociacao {
 
     /**
      * com o uso do "get" separado da palavra, podemos acessa-la usando apenas o nome do método:
-     * variável.volume é a mesma coisa que, variável.getVolume()
+     * variável.volume é a mesma coisa que variável.getVolume()
      */
     get volume() {
         return (this._quantidade * this._valor).toFixed(2);
     }
 
     get data() {
-        return this._data;
+        /**
+         * Prática de programação defensiva, devolve uma nova variável com base no valor da variável presente na classe.
+         * Caso o valor da variável devolvida seja alterada, sua mudança não será refletida no estado da classe.
+         * Esta técnica pode ser empregada em qualquer objeto que tenha métodos que possam alterar seu valor.
+         */
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
@@ -35,7 +40,7 @@ class Negociacao {
 
     /**
      * Quando fora da classe, usamos o prefixo "function".
-     * Dentro da classe, nosso método só precisa do nome.
+     * Dentro da classe nosso método só precisa do nome.
      */
     getValor() {
         return this._valor;
