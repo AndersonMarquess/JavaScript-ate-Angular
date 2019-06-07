08a5732099a1b972aaab0c0c6ba872870dd02d90
unknown <andersongabriel96.ag@gmail.com>
2019-06-07 17:10:01 -0300

    Conversão da tipo, índice do foreach, map e arrow function

--- Curso 2/aluraframe/client/js/controllers/NegociacaoController.js
+++ Curso 2/aluraframe/client/js/controllers/NegociacaoController.js
@@ -4,9 +4,9 @@ class NegociacaoController {
         /**
          * Adiciona a referência ao elemento na variável atributo, evitando buscas constates no DOM.
          */
-        this.data = this.buscarValor("#data");
-        this.quantidade = this.buscarValor("#quantidade");
-        this.valor = this.buscarValor("#valor");
+        this._data = this.buscarValor("#data");
+        this._quantidade = this.buscarValor("#quantidade");
+        this._valor = this.buscarValor("#valor");
     }
 
     /**
@@ -16,9 +16,45 @@ class NegociacaoController {
     adicionar(event) {
         event.preventDefault();
 
-        console.log(this.data.value);
-        console.log(this.quantidade.value);
-        console.log(this.valor.value);
+        let negociacao = new Negociacao(
+            this.obtemDataPorString(this._data.value),
+            this._quantidade.value,
+            this._valor.value
+        );
+        console.log(negociacao);
+    }
+
+    obtemDataPorString(stringData) {
+        let dataSplit = stringData.split("-");
+        let ano = dataSplit[0];
+        /**
+         * dado a string "35" o resultado de "35" - 2 é 33 em number.
+         * O JavaScript faz a conversão da string para number.
+         */
+        let mes = dataSplit[1] - 1;
+        let dia = dataSplit[2];
+        return new Date(ano, mes, dia);
+    }
+
+    /**
+     * Mapeia cada elemento do array para instrução escolhida.
+     * Neste caso, recebe a string "2019-06-30" e retorna {"2019", 05, "30"}, 
+     * considerando o mês como number.
+     * É subtraído 1 do mês, por que o date considera (janeiro = 0) logo, (dezembro = 11).
+     */
+    obtemDataPorStringComMap(stringData) {
+        //o índice está presente no foreach
+        stringData.split("-").map((elemento, indice) => {
+            if (indice == 1) {
+                return elemento - 1;
+            }
+            return elemento;
+        });
+
+        // é o mesmo que :
+        return stringData
+            .split("-")
+            .map((elemento, indice) => elemento - indice % 2); // Uma única instrução, o retorno é automático.
     }
 
     buscarValor(seletor) {
