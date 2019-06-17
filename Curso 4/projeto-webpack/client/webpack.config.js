81b5bf538ed0ddb5a2743e9197132f654b379c24
unknown <andersongabriel96.ag@gmail.com>
2019-06-17 14:35:15 -0300

    configurado build de produção com minificação.

--- Curso 4/projeto-webpack/client/webpack.config.js
+++ Curso 4/projeto-webpack/client/webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+// Verifica se a variável de ambiente chamada NODE_ENV (definado no package.json) possui o valor especificado.
+if(process.env.NODE_ENV == 'production') {
+    plugins.push(new babiliPlugin());
+}
 
 module.exports = {
     // Ponto de entrada, script inicial da aplicação, para resolver os outros.
@@ -20,5 +28,7 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    //é o mesmo que: plugins = plugins, no ES6+ quando a chave tem o mesmo nome que o valor ela pode ser omitida.
+    plugins
 }
