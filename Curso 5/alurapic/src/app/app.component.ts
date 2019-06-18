853566808a872bdcc40ccdc98a4244fad61b349a
unknown <andersongabriel96.ag@gmail.com>
2019-06-18 19:03:15 -0300

    Data binding para atributos e angular expression para tags.

--- Curso 5/alurapic/src/app/app.component.ts
+++ Curso 5/alurapic/src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
+	// Template é o nome dado a parte html.
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
+// nome do arquivo: app.component.ts
+// nome da classe: AppComponent
 export class AppComponent {
 	title = 'alurapic';
+	urlImg = 'https://images.unsplash.com/photo-1560859581-d51f66c66233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80';
+	imgDescricao = 'Costa do mar';
 }
