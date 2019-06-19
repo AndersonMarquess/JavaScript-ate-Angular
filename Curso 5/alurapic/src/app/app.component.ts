d85306a6034f6d86f5fda4a05909370e12506974
unknown <andersongabriel96.ag@gmail.com>
2019-06-19 16:03:46 -0300

    diretiva ngFor

--- Curso 5/alurapic/src/app/app.component.ts
+++ Curso 5/alurapic/src/app/app.component.ts
@@ -11,4 +11,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'alurapic';
 
+	todasAsFotos = [
+		{
+			urlImg: "https://images.unsplash.com/photo-1560859581-d51f66c66233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375",
+			imgDescricao: "Beira do mar dia"
+		},
+		{
+			urlImg: "https://images.unsplash.com/photo-1560932668-46f7a662129e?ixlib=rb-1.2.1&auto=format&fit=crop&w=332",
+			imgDescricao: "Beira do mar tarde"
+		}
+	];
 }
